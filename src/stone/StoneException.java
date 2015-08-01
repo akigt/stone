d0b0fae15c90782bcd0bff98f795d1ec39b8f7e3
Aki <mifukia1101@gmail.com>
2015-08-01 23:13:09 +0900

    chapter3 create script language

---
+++ src/stone/StoneException.java
@@ -0,0 +1,9 @@
+package stone;
+
+public class StoneException extends RuntimeException {
+	public StoneException(String m) { super(m); }
+	public StoneException(String m, ASTree t) {
+		super(m + " " + t.location());
+
+	}
+}
